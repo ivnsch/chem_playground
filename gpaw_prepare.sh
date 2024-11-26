00b31e79e7075384890895e9a2fdabf32d2f442a
Ivan Schuetz <ivanschuetz@gmail.com>
2024-11-26 18:01:56 +0100

    add command to script

--- gpaw_prepare.sh
+++ gpaw_prepare.sh
@@ -2,3 +2,5 @@ export CFLAGS="-I$(brew --prefix libxc)/include"
 export LDFLAGS="-L$(brew --prefix libxc)/lib"
 export LIBRARY_PATH=$(brew --prefix libxc)/lib
 export CC=mpicc
+
+ln -sf $(which python3) /opt/homebrew/bin/python
